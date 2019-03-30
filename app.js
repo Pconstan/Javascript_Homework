8860ae47abc057ef9929367030f68f16bc9f328d
Pconstan <pconstantelos@gmail.com>
2019-03-30 01:24:20 -0500

    completed hw

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ var shapes = [];
 var durationsMins = [];
 var comments_ = [];
 
+var tbody = d3.select("tbody");
+
 tableData.forEach((info) => {
     Object.entries(info).forEach(([key, value]) => {
 
@@ -36,3 +38,40 @@ tableData.forEach((info) => {
         }
     });
 });
+    tableData.forEach(function(info) {
+        console.log(info);
+        var row = tbody.append("tr");
+        Object.entries(info).forEach(function([key, value]){
+            console.log(key, value);
+            var cell = tbody.append("td");
+            cell.text(value);
+        });
+});
+
+//
+
+var inputText = d3.select("#datetime")
+var btn = d3.select("#filter-btn")
+
+function displayData(data){ 
+    tbody.text("")
+    data.forEach(function(info){
+    new_tr = tbody.append("tr")
+    Object.entries(info).forEach(function([key, value]){
+        new_td = new_tr.append("td").text(value)	
+    });
+})};
+
+
+
+// filter data with desired date
+function changeHandler(){
+    d3.event.preventDefault();
+    console.log(inputText.property("value"));
+    var new_tbl = tableData.filter(info => info.datetime===inputText.property("value"))
+    displayData(new_tbl);
+}
+
+// event listener to handle change and click
+inputText.on("change", changeHandler);
+btn.on("click", changeHandler);
